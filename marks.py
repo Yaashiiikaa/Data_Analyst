773d05316380101f9c2f4d4b784d9b73115488a7
Yaashiiikaa <22cs123@drngpit.ac.in>
2026-07-18 12:21:29 +0530

    Added Python Day 1 to Day 2 programs

---
+++ marks.py
@@ -0,0 +1,11 @@
+marks=85
+if marks>=90 or marks<=100:
+   print("A+")
+elif marks>=80 or marks<=89:
+   print("A")
+elif marks>=70 or marks<=79:
+   print("B")
+elif marks>=60 or marks<=69:
+   print("C")
+else:
+    print("Fail")
